939e9cc1df25d512459f444c21b7a983943e584e
Brandon Maxwell <theill1b22@gmail.com>
2022-01-25 14:03:00 -0600

    added pages for Login, and the css for Login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
+import { useContext } from 'react';
+import { Routes, Route } from 'react-router-dom'
 // Components
 import Nav from './components/Nav';
 // Pages
+import Login from './pages/Login';
 // Contexts
+import UserContext from './contexts/UserContext';
 // CSS
 import './App.css';
 
+
 function App() {
+  // const user = useContext(UserContext)
+  // console.log(user)
   return (
     <div className="App">
-            
-            <Nav />
+      
+      <UserContext.Provider value={"Brandon"}>
+
+        
+        <Nav />
+
+        <Routes>
+          <Route path='login' element={<Login />} />
+        </Routes>
+
+
+
+      </UserContext.Provider>
+
     </div>
   );
 }
---
+++ src/pages/Login/index.js
@@ -0,0 +1,20 @@
+import './styles.css'
+
+const Login = () => {
+    return (
+        <form className="mx-auto border p-2 m-2" id="login-form">
+            <div className="mb-3">
+                <label htmlFor="exampleInputUser1" className="form-label">Username</label>
+                <input type="username" className="form-control" id="exampleInputUser1" aria-describedby="userHelp" />
+                    <div id="userHelp" className="form-text">We'll never share your username with anyone else.</div>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" />
+            </div>            
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+    );
+}
+
+export default Login;
